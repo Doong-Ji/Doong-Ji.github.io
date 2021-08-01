907c3c5c4ae5083f9381fa32198289bab368453c
snr <nr.shin000@gmail.com>
2021-08-02 00:13:56 +0900

    feat : public folder pu ( html, css ) upload - about_member - 어바웃 - 멤버 페이지 - member_project -어바웃 - 멤버 - 참여 프로젝트 페이지

--- client/public/pu/about.js
+++ client/public/pu/about.js
@@ -2,14 +2,34 @@ var swiper = new Swiper('.mySwiper', {
   slidesPerView: 2,
   breakpoints: {
     // when window width is <= 499px
-    499: {
-      slidesPerView: 1,
+    1200: {
+      slidesPerView: 2,
       spaceBetweenSlides: 30,
     },
     // when window width is <= 999px
-    999: {
+    768: {
       slidesPerView: 1,
       spaceBetweenSlides: 40,
+    },
+  },
+});
+
+var reviewSwiper = new Swiper('.reviewSwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 3,
+  breakpoints: {
+    // when window width is <= 499px
+    1200: {
+      slidesPerView: 2,
+      spaceBetweenSlides: 30,
+    },
+    // when window width is <= 999px
+    768: {
+      slidesPerView: 1,
+      spaceBetweenSlides: 100,
     },
   },
 });
