30ea8c856b377c99b09aec46c0db8bc6cca315d2
snr <nr.shin000@gmail.com>
2021-08-15 01:34:43 +0900

    feat : pu project page , common file

---
+++ client/public/pu/project.js
@@ -0,0 +1,37 @@
+// var swiper = new Swiper('.projectSwiper', {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   breakpoints: {
+//     // when window width is <= 499px
+//     1200: {
+//       slidesPerView: 2,
+//       spaceBetween: 30,
+//     },
+//     // when window width is <= 999px
+//     768: {
+//       slidesPerView: 1,
+//       spaceBetween: 40,
+//     },
+//   },
+// });
+
+var reviewSwiper = new Swiper('.projectSwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 3,
+  spaceBetween: 22,
+  breakpoints: {
+    // when window width is <= 1200px
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 22,
+    },
+    // when window width is <= 768px
+    768: {
+      slidesPerView: 1.7,
+      spaceBetween: 10,
+    },
+  },
+});
